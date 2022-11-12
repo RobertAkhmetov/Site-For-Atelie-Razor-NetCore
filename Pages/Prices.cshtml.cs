df45039b448270a68286639e5588c251b5f1eb00
RobertAkhmetov <leadersclub777@gmail.com>
2022-11-12 14:13:18 +0300

    12-11-remade

---
+++ Pages/Prices.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SiteForAtelieRazorNetCore.Pages
+{
+    public class PricesModel : PageModel
+    {
+        public string Message { get; set; }
+
+        public void OnGet()
+        {
+            Message = "тут цены будут указаны, повторно.";
+        }
+    }
+}
--- README.md
+++
@@ -1,1 +0,0 @@
-this directory is website for Atelie "Guli", which realized in visual studio (c# MVC) with using Razor Pages
